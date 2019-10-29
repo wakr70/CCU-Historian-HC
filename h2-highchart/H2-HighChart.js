602981e69e24051b7cd78bb4316319749596ddba
wakr70 <wakr@aon.at>
2019-10-29 20:12:22 +0100

    Update Version to v3.8

--- h2-highchart/H2-HighChart.js
+++ h2-highchart/H2-HighChart.js
@@ -9,7 +9,7 @@ var H2_refreshSec = 60;
 // Refresh Time is enabled
 
 // declare global Variables
-var H2_version = 'v3.7';
+var H2_version = 'v3.8';
 var chart;
 var filter_feld = '';
 var DP_point = [];
