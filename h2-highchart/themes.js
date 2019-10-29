c62b59c73c8c7639c3bdad4eafe1acfc5a04bef1
wakr70 <wakr@aon.at>
2019-10-29 13:42:19 +0100

    Add new themes for transparent and dark-transp

--- h2-highchart/themes.js
+++ h2-highchart/themes.js
@@ -1495,15 +1495,263 @@ var DP_Themes = {
 
 'transparent': {
 	chart: {
-	    backgroundColor: 'rgba(255,255,255, .5)',
+	    backgroundColor: 'rgba(255, 255, 255, 0.0)',
+	    plotBackgroundColor: 'rgba(255,255,255, .1)',
         plotShadow: false,
 	    style: {
 	        fontFamily: 'Dosis, sans-serif'
 	    }
 	},
+    tooltip: {
+        backgroundColor: 'rgba(247,247,247,0.7)'
+    },
+	legend: {
+        borderColor: null,
+        borderRadius: 0,
+        borderWidth: 0,
+        // backgroundColor not working, 
+        backgroundColor2: 'rgba(255, 255, 255, 0.0)'
+    },
+ 
+  },
+'dark-transp': {
+	chart: {
+	    backgroundColor: 'rgba(255, 255, 255, 0.0)',
+	    plotBackgroundColor: 'rgba(255,255,255, .1)',
+        plotShadow: false,
+	    style: {
+	        fontFamily: 'Dosis, sans-serif'
+	    }
+	},
+    title: {
+      style: {
+          color: '#C0C0C0',
+             }
+    },
+    subtitle: {
+      style: {
+          color: '#666666',
+      }
+    },
+	  xAxis: {
+	      gridLineColor: '#333333',
+	      gridLineWidth: 1,
+	      labels: {
+	          style: {
+	              color: '#A0A0A0'
+	          }
+	      },
+	      lineColor: '#A0A0A0',
+	      tickColor: '#A0A0A0',
+	      title: {
+	          style: {
+	              color: '#CCC',
+	              fontWeight: 'bold',
+	              fontSize: '12px',
+	              fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+	
+	          }
+	      }
+	  },
+	  yAxis: {
+	      gridLineColor: '#333333',
+	      labels: {
+	          style: {
+	              color: '#A0A0A0'
+	          }
+	      },
+	      lineColor: '#A0A0A0',
+	      minorTickInterval: null,
+	      tickColor: '#A0A0A0',
+	      tickWidth: 1,
+	      title: {
+	          style: {
+	              color: '#CCC',
+	              fontWeight: 'bold',
+	              fontSize: '12px',
+	              fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+	          }
+	      }
+	  },
+	  tooltip: {
+	      backgroundColor: 'rgba(247,247,247,0.7)',
+	      style: {
+	          color: '#F0F0F0'
+	      }
+	  },
+	  toolbar: {
+	      itemStyle: {
+	          color: 'silver'
+	      }
+	  },
+	  plotOptions: {
+	      line: {
+	          dataLabels: {
+	              color: '#CCC'
+	          },
+	          marker: {
+	              lineColor: '#333'
+	          }
+	      },
+	      spline: {
+	          marker: {
+	              lineColor: '#333'
+	          }
+	      },
+	      scatter: {
+	          marker: {
+	              lineColor: '#333'
+	          }
+	      },
+	      candlestick: {
+	          lineColor: 'white'
+	      }
+	  },
+	  legend: {
+	      borderColor: null,
+	      borderRadius: 0,
+	      borderWidth: 0,
+	      // backgroundColor not working, 
+	      backgroundColor2: 'rgba(255, 255, 255, 0.0)',
+	      itemStyle: {
+	          color: '#A0A0A0'
+	      },
+	      itemHoverStyle: {
+	          color: '#FFFFFF',
+	          fontWeight: 'bold'
+	      },
+	      itemHiddenStyle: {
+	    	  color: '#606060'
+	      }
+	  },
+	  credits: {
+	      style: {
+	          color: '#666'
+	      }
+	  },
+	  labels: {
+	      style: {
+	          color: '#CCC'
+	      }
+	  },
+	
+	
+	  navigation: {
+	      buttonOptions: {
+	          symbolStroke: '#DDDDDD',
+	          hoverSymbolStroke: '#FFFFFF',
+	          theme: {
+	              fill: {
+	                  linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	                  stops: [
+	                      [0.4, '#606060'],
+	                      [0.6, '#333333']
+	                  ]
+	              },
+	              stroke: '#000000'
+	          }
+	      }
+	  },
 
-    // General
-    background2: 'rgba(255,255,255, .5)'
-    	
-   }
+	  // scroll charts
+	  rangeSelector: {
+	      buttonTheme: {
+	          fill: {
+	              linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	              stops: [
+	                  [0.4, '#888'],
+	                  [0.6, '#555']
+	              ]
+	          },
+	          stroke: '#000000',
+	          style: {
+	              color: '#CCC',
+	              fontWeight: 'bold'
+	          },
+	          states: {
+	              hover: {
+	                  fill: {
+	                      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	                      stops: [
+	                          [0.4, '#BBB'],
+	                          [0.6, '#888']
+	                      ]
+	                  },
+	                  stroke: '#000000',
+	                  style: {
+	                      color: 'white'
+	                  }
+	              },
+	              select: {
+	                  fill: {
+	                      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	                      stops: [
+	                          [0.1, '#000'],
+	                          [0.3, '#333']
+	                      ]
+	                  },
+	                  stroke: '#000000',
+	                  style: {
+	                      color: 'yellow'
+	                  }
+	              }
+	          }
+	      },
+	      inputStyle: {
+	          backgroundColor: '#333',
+	          color: 'silver'
+	      },
+	      labelStyle: {
+	          color: 'silver'
+	      }
+	  },
+
+	  navigator: {
+	      handles: {
+	          backgroundColor: '#666',
+	          borderColor: '#AAA'
+	      },
+	      outlineColor: '#CCC',
+	      maskFill: 'rgba(16, 16, 16, 0.5)',
+	      series: {
+	          color: '#7798BF',
+	          lineColor: '#A6C7ED'
+	      }
+	  },
+
+	  scrollbar: {
+	      barBackgroundColor: {
+	          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	          stops: [
+	              [0.4, '#888'],
+	              [0.6, '#555']
+	          ]
+	      },
+	      barBorderColor: '#CCC',
+	      buttonArrowColor: '#CCC',
+	      buttonBackgroundColor: {
+	          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	          stops: [
+	              [0.4, '#888'],
+	              [0.6, '#555']
+	          ]
+	      },
+	      buttonBorderColor: '#CCC',
+	      rifleColor: '#FFF',
+	      trackBackgroundColor: {
+	          linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+	          stops: [
+	              [0, '#000'],
+	              [1, '#333']
+	          ]
+	      },
+	      trackBorderColor: '#666'
+	  },
+	
+	  // special colors for some of the
+	  dataLabelsColor: '#444',
+	  textColor: '#C0C0C0',
+	  maskColor: 'rgba(255,255,255,0.3)'
+	       
+	  }
 };
